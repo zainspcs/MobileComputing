ad6779f6128210f842657ee953f99510dfdd04d1
Zain <zain_mughal@ymail.com>
2015-02-26 20:23:57 +0500

    vers2

---
+++ google.maps.js
@@ -0,0 +1,16 @@
+/*
+ * Google Maps documentation: http://code.google.com/apis/maps/documentation/javascript/basics.html
+ * Geolocation documentation: http://dev.w3.org/geo/api/spec-source.html
+ */
+(document).ready(function() {
+    initialize();
+});
+  function initialize() {
+    var latlng = new google.maps.LatLng(40.7562008,-73.9903784);
+    var myOptions = {
+      zoom: 18,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  }
